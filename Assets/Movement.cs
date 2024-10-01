54fd9b05ac71cb1e9087b636fad59be677ea3005
T-a-r-a-n-P <taranpinnick@gmail.com>
2024-10-01 10:15:58 +0100

    Initial commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    public float moveSpeed = 5f;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+       float moveInputX = Input.GetAxisRaw("Horizontal");
+       float moveInputY = Input.GetAxisRaw("Vertical");
+
+       Vector2 moveDirection = new Vector2(moveInputX, moveInputY);
+       moveDirection.Normalize();
+       rb2d.velocity = moveDirection * moveSpeed; 
+    }
+}
