4de2797439dabd0a131961969bed38577b6ff1e7
T-a-r-a-n-P <taranpinnick@gmail.com>
2025-02-03 22:23:46 +0000

    pause menu

--- Assets/MyScripts/UImanager.cs
+++ Assets/MyScripts/UImanager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class UImanager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject pauseMenuUI;
+    public static bool isPaused = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused) ResumeGame();
+            else PauseGame();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting the game");
+        Application.Quit();
     }
 }
